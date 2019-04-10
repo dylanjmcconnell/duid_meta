7fc261738c0315e51cb45a8dee067b5785471690
Dylan McConnell <dylan-climate-energy-college@users.noreply.github.com>
2019-04-10 20:00:24 +1000

    comment out facility mapping in npi module - for now

--- npi.py
+++ npi.py
@@ -19,7 +19,7 @@ def load_facility(facility_id="Q019SIE001", filename="test_npi.csv"):
 
 def load_npi_data(select=SQLITE):
     path = os.path.join(MODULE_DIR, "data","npi_data.csv")
-    df = pd.read_csv(path)    
+    df = pd.read_csv(path)
     df.rename(columns={col_name:col_name.upper() for col_name in df}, inplace=True)
 
     #map subtances
@@ -27,7 +27,23 @@ def load_npi_data(select=SQLITE):
     key_map = substance_keys.to_dict(orient='dict')['ID']
     df['SUBSTANCE_NAME'] = df['SUBSTANCE_NAME'].apply(lambda x: key_map[x])
 
-    #map facilities
+    #map facilities    
+
+    #duid_keys = pd.read_sql("SELECT DUID, ID FROM DU", con=select, index_col="DUID")
+    #station_keys = pd.read_sql("SELECT DUID, STATIONID FROM DUDETAILSUMMARY", con=select, index_col="DUID")
+
+    #key_map = station_keys.to_dict(orient='dict')['STATIONID']
+
+    #dx['SID'] = dx.ID.apply(lambda x: station_keymap(x, key_map))
+
+    #key_map = substance_keys.to_dict(orient='dict')['ID']
+    #df['SUBSTANCE_NAME'] = df['SUBSTANCE_NAME'].apply(lambda x: key_map[x])
 
     return df[['REPORT_ID', 'REPORT_YEAR', 'FACILITY_ID', 'SUBSTANCE_NAME', 'AIR_POINT_EMISSION_KG',
                'AIR_FUGITIVE_EMISSION_KG', 'AIR_TOTAL_EMISSION_KG', 'WATER_EMISSION_KG', 'LAND_EMISSION_KG']]
+
+def station_keymap(x, keymap):
+    try:
+        return keymap[x]
+    except:
+        pass
