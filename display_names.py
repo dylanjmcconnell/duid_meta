3a668d3bed3f75eaff6b495b8996c49f91efc7cb
Dylan McConnell <dylan-climate-energy-college@users.noreply.github.com>
2019-04-10 13:24:36 +1000

    Display names refactor

--- display_names.py
+++ display_names.py
@@ -1,3 +1,14 @@
+import os
+import pandas as pd
+from duid_meta import MODULE_DIR
+
+def load_and_parse():
+    path = os.path.join(MODULE_DIR, "data","display_names.csv")
+    df = pd.read_csv(path)
+    df.DISPLAYNAME = df.STATIONNAME.apply(display_names)
+    dx = exclude(df)
+    dx.to_csv(path, index=None)
+
 def display_names(x):
     display_str = x.title()
     for name in ["Power Station",
@@ -5,6 +16,7 @@ def display_names(x):
                 "Windfarm",
                 "Solar Farm",
                 "Solar Park",
+                "Solar Pv",
                 "Solar Project",
                 "Power Plant",
                 "Solar Plant",
@@ -15,11 +27,15 @@ def display_names(x):
                 "Combined Cycle",
                 "Facility",
                 "Plant",
-                "Stage",
+                "Stage ",
                 "Station",
                 "Diesel",
                 "Generator",
                 "Mountain Streams",
+                "Hydro",
+                "Pumps",
+                "Power",
+                "Ps",
                 '"',
                 "'"]:
         display_str = display_str.replace(name,"")
@@ -27,27 +43,32 @@ def display_names(x):
     for name, replace in {"Landfill Gas": "LFG",
                           "Nsw": "NSW",
                           "Agl": "AGL",
-                          "Vic": "VIC ",
+                          "Vic": "VIC",
                           "Sa ": "SA ",
                           "Hrl": "HRL",
                           "Lfg": "LFG",
                           "Krc": "KRC" ,
                           "Scsf": "SCSF",
                           "Renewable Energy": "RE Facility",
-                          "Waste Coal Mine Gas":"WCMG"}.items():
+                          "Waste Coal Mine Gas":"WCMG",
+                          "Battery Energy Storage System" : "Battery",
+                          "Energy Storage System": "Battery",
+                          "Mini" : "(Mini Hydro)",
+                          "Small": "(Mini Hydro)",
+                          "Cogeneration": "Co-gen"}.items():
         display_str=display_str.replace(name, replace)
 
     #special_case
     for case, replace in {"Swanbank B  & Swanbank E" : "Swanbank",
                           "Amcor Glass, Gawler Plant": "Amcor Glass, Gawler",
-                          "Hepburn Community": "Hepburn wind",
+                          "Hepburn Community": "Hepburn Wind",
                           "Basslink Hvdc Link": "Basslink",
                           "Ballarat Base Hospital Plant" : "Ballarat Hospital",
                           "Bankstown Sports Club Plant Units" : "Bankstown Sports Club",
                           "Woodlawn Bioreactor Energy Generation": "Woodlawn Bioreactor",
                           "Wollert Renewable Energy " : "Wollert RE Facility",
                           "Woolnorth Studland Bay / Bluff Point" : "Woolnorth",
-                          'Shoalhaven  (Bendeela And Kangaroo Valley  And Pumps)': "Shoalhaven",
+                          'Shoalhaven  (Bendeela And Kangaroo Valley  And )': "Shoalhaven",
                           "Wingfield 1 Landfill": "Wingfield (LFG)",
                           "Wingfield 2 Landfill": "Wingfield (LFG)",
                           "Woy Woy Landfill ": "Woy Woy (LFG)",
@@ -59,7 +80,7 @@ def display_names(x):
                           "Lemonthyme / Wilmot" : "Lemonthyme/Wilmot",
                           "Callide C Nett Off" : "Callide C",
                           "Eastern Creek 2 Gas Utilisation" : "Eastern Creek LFG",
-                          "Eastern Creek LFG Ps Units 1-4" : "Eastern Creek LFG",
+                          "Eastern Creek LFG  Units 1-4" : "Eastern Creek LFG",
                           "Hallam Road Renewable Energy" : "Hallam Rd RE Facility",
                           "Grosvenor 1 Waste Coal Mine Gas" : "Grosvenor",
                           "Isis Central Sugar Mill Co-Generation" : "Isis Central Sugar Mill",
@@ -86,21 +107,44 @@ def display_names(x):
                           "Snowtown  2 North": "Snowtown North",
                           "Hume (NSW) Hydro" : "Hume Hydro (NSW)",
                           "Hume (VIC ) Hydro" : "Hume Hydro (VIC)",
-                          "South East Water - Halllam Hydro" : "Halllam Hydro"
+                          "South East Water - Halllam Hydro" : "Halllam Hydro",
+                          "Ross River , Units 1-64" : "Ross River",
+                          "Tailem Bend  1, Units 1-54" : "Tailem Bend",
+                          "Wemen , Units 1-39": "Wemen",
+                          "Murray1" : "Murray 1",
+                          "Murray2" : "Murray 2",
+                          "Tumut1" : "Tumut 1",
+                          "Tumut2" : "Tumut 2",
+                          "Tumut3" : "Tumut 3",
+                          "VICtoria Mill" : "Victoria Mill"
                           }.items():
         if case  in  display_str: 
             display_str = replace
-    display_str =  display_str.strip()
-    special_cases = ["Bhas Zinc Plant SA",
-                     "Victorian Smelter Ancillary Services", 
-                     "Northern Gas Turbine",
-                     "DLINK",
-                     "RESERVE TRADER NSW1",
-                     "Murraylink",
-                     "NSW1 Dummy Generator",
-                     "Basslink",
-                     "Station ID - Dummy (NSCAS)",
-                     "Enoc Msap VIC",
-                     "Traralgon Network Support Station",
-                     "ERGON FREQUENCY CONTROL A.S."]
-    return display_str
+    return display_str.strip()
+
+def exclude(df):
+    exclude_list = ["Reserve Trader NSW1",
+                    "Reserve Trader Qld1",
+                    "Reserve Trader Sa1",
+                    "Reserve Trader VIC1",
+                    "Bhas Zinc  Sa",
+                    "VICtorian Smelter Ancillary Services",
+                    "NSW1 Dummy",
+                    "Qld1 Dummy",
+                    "Sa1 Dummy",
+                    "VIC1 Dummy",
+                    "Tas1 Dummy",
+                    "Reserve Trader Tas1",
+                    "Basslink",
+                    "Murraylink",
+                    "Directlink",
+                    "Id - Dummy (Nscas)",
+                    "Traralgon Network Support Station"
+                    "Ergon FCAS",
+                    "Enoc Msap NSW",
+                    "Enoc Masp Qld",
+                    "Enoc Msap VIC"]
+
+    other = [       "Northern Gas Turbine"]
+
+    return df[~df.DISPLAYNAME.isin(exclude_list)]
